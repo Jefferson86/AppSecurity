cda6820b4512d2b11435127f3740db5d9bb0cbb4
jefferson <jmirandaa86@gmail.com>
2016-10-26 21:53:01 -0500

    Actualizada 2

---
+++ app/src/main/java/security/cbc/com/appsecurity/configuracion.java
@@ -0,0 +1,13 @@
+package security.cbc.com.appsecurity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class configuracion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracion);
+    }
+}
